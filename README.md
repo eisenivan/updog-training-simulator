3b625b7c92bd8efc54430b9a75649bbfcc5a2ad7
Ivan Eisenberg <ivaneisenberg@northwesternmutual.com>
2019-10-01 15:23:52 -0500

    updog initiate

---
+++ README.md
@@ -0,0 +1,13 @@
+# Updog Training Simulator
+
+## To Play
+
+Clone the repo
+
+`cd ./updog-training-simulator`
+
+`node ./updog.js`
+
+## Contributing
+
+We're looking for prompts. Be creative. The three types of clues we are looking for are `Up<something>` ex. `Updraft`, `<something>dog` ex. `hotdog`, and `<direction><animal>` ex. `left shark`. Any submissions will be considered however. If you think it might be a stretch, feel free to include a comment as to why your clue works.
---
+++ updog.js
@@ -0,0 +1,67 @@
+const readline = require('readline')
+const prompts = require('./prompts.json')
+
+const intro = async () => {
+  console.log(`
+                        
+_____     ____          
+|  |  |___|   \\ ___ ___ 
+|  |  | . |  |  | . | . |
+|_____|  _|____/|___|_  |
+      |_|           |___|
+
+The training simulator
+
+Instructions:
+
+You will be provided with a potential definition of "Updog".
+But the answer will not be updog. Your challenge is to solve
+each clue by responding with the word the clue is referencing.
+
+As an example, if the prompt was "I think updog is a word you
+would use to describe a positive trend.
+
+You would respond with "upward"
+
+Let's give it a try.
+
+
+`)
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+let i = 0
+
+const ask = async prompt => {
+  if (i === 0) {
+    await intro()
+  }
+
+  rl.question(`I think "updog" is ${prompt.clue}.\n\n>> `, (answer) => {
+    if (answer.toLowerCase() === prompt.word.toLowerCase()) {
+      console.log(`\nAh, that\'s right, that is ${answer}`)
+      console.log('')
+      i++
+    } else {
+      console.log('No that\'s not it. I\'m pretty sure it\'s updog')
+      console.log('')
+    }
+
+    if (prompts[i]) {
+      setTimeout(() => {
+        ask(prompts[i])
+      }, 1000)
+    } else {
+      console.log('')
+      console.log('Huzzah! You are now an updog master. Go forth and conquer!!')
+      rl.close()
+    }
+  })
+}
+
+// start the game
+ask(prompts[i])
